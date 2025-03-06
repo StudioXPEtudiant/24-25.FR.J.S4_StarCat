ee24c54a48fa93c539e93599966ea7b230273b69
ypixell <166909753+ypixell@users.noreply.github.com>
2025-03-06 20:37:11 +0100

    Double saut + logo

--- script/main_character.gd
+++ script/main_character.gd
@@ -3,17 +3,18 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -350.0
-
+var  max_jumps: int = 1
+var jumps_left: int = max_jumps
 
 func _physics_process(delta: float) -> void:
 	
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jumps_left > 0:
 		velocity.y = JUMP_VELOCITY
+		jumps_left -= 1
 	if Input.is_action_just_pressed("Echap"):
 		print("Echap")
 		get_tree().change_scene_to_file("res://scene/main_menu.tscn")
@@ -25,7 +26,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, 20)
-		
+	if is_on_floor():
+		jumps_left = max_jumps
 	move_and_slide()
 	 
 	var collision = move_and_collide(velocity * delta)
