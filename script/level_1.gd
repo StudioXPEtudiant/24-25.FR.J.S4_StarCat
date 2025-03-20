7f5d840d01813475468c4eac9bd2c00274dc06b8
ypixell <166909753+ypixell@users.noreply.github.com>
2025-03-20 19:56:05 +0100

    Eau 1

---
+++ script/level_1.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Sandbox"):
+		get_tree().change_scene_to_file("res://scene/test_programmer.tscn")
