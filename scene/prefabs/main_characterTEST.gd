4810ac1c485aa569116c551ad1936b42316c3c14
diego <Xouxou722@gmailcom>
2025-05-15 19:27:17 +0200

    niveau 1 chart test corige bug

--- scene/prefabs/main_characterTEST.gd
+++ scene/prefabs/main_characterTEST.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 350.0
-const JUMP_VELOCITY = -450.0
+const JUMP_VELOCITY = -700.0
 var  max_jumps: int = 1
 var jumps_left: int = max_jumps
 var max_vie: int = 3
